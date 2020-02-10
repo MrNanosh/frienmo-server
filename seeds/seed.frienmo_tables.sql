ab6ccaf41a4599e8b2841bc27f0e5d5c6c280de4
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-02-10 16:14:26 -0500

    added admin and friend admin

---
+++ seeds/seed.frienmo_tables.sql
@@ -0,0 +1,20 @@
+Begin;
+
+TRUNCATE
+    user
+    RESTART IDENTITY CASCADE;
+
+INSERT INTO user ( 
+    username, password,real_name,phone
+)
+VALUES
+('admin','pass','real admin','734123456'),
+('friend admin', 'passadmin', 'friend admin', '7341234587');
+
+INSERT INTO friend(
+    user_id, friend_id, accepted
+)
+VALUES
+( 1, 2, true);
+
+COMMIT;
