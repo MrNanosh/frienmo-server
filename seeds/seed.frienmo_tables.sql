c54817c639c360576142b2adc4621b9d892a8ff6
Jack <pupeljack@gmail.com>
2020-02-14 15:57:11 -0600

    set up basic structure for seeding ALL tables

--- seeds/seed.frienmo_tables.sql
+++ seeds/seed.frienmo_tables.sql
@@ -1,23 +1,55 @@
 Begin;
 
 TRUNCATE
-
-    "user"
+    "user",
+    "friend",
+    "favor",
+    "outstanding",
+    "review",
+    "tags",
+    "tagged"
     RESTART IDENTITY CASCADE;
 
 INSERT INTO "user" ( 
     "username", "password","name","phone"
 )
 VALUES
-('admin','pass','real admin','734123456'),
-('friend admin', 'passadmin', 'friend admin', '7341234587');
+('test 1', 'test-1-pass', 'Test 1', '734123456'),
+('test 2', 'test-2-pass', 'Test 2', '734123456'),
+('test 3', 'test-3-pass', 'Test 3', '734123456'),
+('test 4', 'test-4-pass', 'Test 4', '734123456'),
+('test 5', 'test-5-pass', 'Test 5', '734123456');
 
-INSERT INTO "friend"(
-    "user_id", "friend_id", "accepted"
-)
+INSERT INTO "friend"("user_id", "friend_id", "accepted")
 VALUES
 ('1', '2', 'true'),
-('2','1', 'true');
+('2','1', 'true'),
+('1','3', 'true'),
+('3','1', 'false');
 
+INSERT INTO "favor"(--stuff)
+VALUES
+--stuff
+;
+
+INSERT INTO "outstanding"(--stuff)
+VALUES
+--stuff
+;
+
+INSERT INTO "review"(--stuff)
+VALUES
+--stuff
+;
+    
+INSERT INTO "tags"(--stuff)
+VALUES
+--STUFF
+;
+
+INSERT INTO "tagged"(--STUFF)
+VALUES
+---STUFF
+;
 
 COMMIT;
