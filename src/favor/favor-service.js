a61f3980d9e9814fc20e5b7f519e84c262a32bba
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-02-17 10:11:29 -0800

    fixed logic of get public to only get public favors that they are directly involved in

--- src/favor/favor-service.js
+++ src/favor/favor-service.js
@@ -421,84 +421,103 @@ const FavorService = {
   ) {
     const offset =
       productsPerPage * (page - 1);
-    return db('outstanding as o')
-      .join(
-        'favor as fa',
-        'fa.id',
-        '=',
-        'o.favor_id'
-      )
-      .join(
-        'user as creator',
-        'creator.id',
-        '=',
-        'fa.creator_id'
-      )
-      .where(
-        'fa.publicity',
-        '=',
-        'public'
-      )
-      .join('friend as fr', function() {
-        this.on(
-          'fr.user_id',
+    return (
+      db('outstanding as o')
+        .join(
+          'favor as fa',
+          'fa.id',
           '=',
-          'o.users_id'
-        ).orOn(
-          'fr.friend_id',
+          'o.favor_id'
+        )
+        .join(
+          'user as creator',
+          'creator.id',
+          '=',
+          'fa.creator_id'
+        )
+        .where(
+          'fa.publicity',
+          '=',
+          'public'
+        )
+        // .join('friend as fr', function() {
+        //   this.on(
+        //     'fr.user_id',
+        //     '=',
+        //     'o.users_id'
+        //   ).orOn(
+        //     'fr.friend_id',
+        //     '=',
+        //     'o.receiver_id'
+        //   );
+        // })
+        // .where(function() {
+        //   this.where(
+        //     'fr.user_id',
+        //     '=',
+        //     user_id
+        //   ).orWhere(
+        //     'fr.friend_id',
+        //     '=',
+        //     user_id
+        //   );
+        // })
+        .leftOuterJoin(
+          'user as receiver',
+          'receiver.id',
           '=',
           'o.receiver_id'
-        );
-      })
-      .where(function() {
-        this.where(
-          'fr.user_id',
+        )
+        .leftOuterJoin(
+          'user as issuer',
+          'o.users_id',
           '=',
-          user_id
-        ).orWhere(
-          'fr.friend_id',
-          '=',
-          user_id
-        );
-      })
-      .leftOuterJoin(
-        'user as receiver',
-        'receiver.id',
-        '=',
-        'o.receiver_id'
-      )
-      .leftOuterJoin(
-        'user as issuer',
-        'o.users_id',
-        '=',
-        'issuer.id'
-      )
-      .orderBy('posted', 'desc')
-      .select(
-        'fa.id as favor_id',
-        'fa.title as title',
-        'fa.description as description',
-        'fa.category as category',
-        'fa.expiration_date as expiration_date',
-        'fa.publicity as publicity',
-        'fa.user_location as user_location',
-        'fa.tags as tags',
-        'fa.limit as limit',
-        'o.id as outstanding_id',
-        'o.receiver_redeemed as receiver_redeemed',
-        'o.giver_redeemed as issuer_redeemed',
-        'creator.id as creator_id',
-        'creator.name as creator_name',
-        'creator.username as creator_username',
-        'issuer.id as issuer_id',
-        'issuer.name as issuer_name',
-        'issuer.username as issuer_username',
-        'receiver.id as receiver_id',
-        'receiver.name as receiver_name',
-        'receiver.username as receiver_username'
-      )
-      .limit(productsPerPage)
-      .offset(offset);
+          'issuer.id'
+        )
+        .where(function() {
+          this.where(
+            'fa.creator_id',
+            '=',
+            user_id
+          )
+            .orWhere(
+              'issuer.id',
+              '=',
+              user_id
+            )
+            .orWhere(
+              'receiver.id',
+              '=',
+              user_id
+            );
+        })
+        .orderBy('posted', 'desc')
+        .select(
+          'fa.id as favor_id',
+          'fa.title as title',
+          'fa.description as description',
+          'fa.category as category',
+          'fa.expiration_date as expiration_date',
+          'fa.publicity as publicity',
+          'fa.user_location as user_location',
+          'fa.tags as tags',
+          'fa.limit as limit',
+          'o.id as outstanding_id',
+          'o.receiver_redeemed as receiver_redeemed',
+          'o.giver_redeemed as issuer_redeemed',
+          'creator.id as creator_id',
+          'creator.name as creator_name',
+          'creator.username as creator_username',
+          'issuer.id as issuer_id',
+          'issuer.name as issuer_name',
+          'issuer.username as issuer_username',
+          'receiver.id as receiver_id',
+          'receiver.name as receiver_name',
+          'receiver.username as receiver_username'
+        )
+        .limit(productsPerPage)
+        .offset(offset)
+    );
   },
   updateOutstanding(
     db,
