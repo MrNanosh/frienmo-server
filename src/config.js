02656a12a0b67b68d8c3c81e49aaa698fa24a408
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-02-25 11:34:31 -0500

    added build,deploy,and,postdeploy to packagdotjson. added to config and env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
-module.exports = {
+export default {
+  TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
+  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
+  SKIP_PREFLIGHT_CHECK: process.env.REACT_APP_PREFLIGHT_CHECK,
   PORT: process.env.PORT || 8000,
   NODE_ENV:
     process.env.NODE_ENV ||
